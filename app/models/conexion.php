d0c957807b2be589c168bab6c507384f4fe65104
fragolan <fragolan.sistemas@gmail.com>
2025-08-19 18:30:51 -0600

    Edicion Canales
    Se termina la edicion de los canales

--- app/models/conexion.php
+++ app/models/conexion.php
@@ -5,11 +5,6 @@ $username = "fragcom_develop"; // Usuario de MySQL
 $password = "S15t3ma5@Fr4g0l4N"; // Contraseña de MySQL
 $database = "fragcom_linking_people"; // base de datos
 
-// $servername = "localhost"; // Servidor de base de datos
-// $username = "root"; // Usuario de MySQL
-// $password = ""; // Contraseña de MySQL
-// $database = "fragcom_ventas_test"; // base de datos
-
 
 // $database = "fragcom_develop"; // base de datos
 
--- app/views/canales/editar.php
+++ app/views/canales/editar.php
@@ -2,15 +2,15 @@
 
     <h1>Editar Canal</h1>
 
-    <form action="/canalestiendas/update" method="POST">
-        <input type="hidden" name="id" value="<?php echo $tienda['id']; ?>">
+    <form action="/canales/update" method="POST">
+        <input type="hidden" name="id" value="<?php echo $canal['id']; ?>">
         <div class="mb-3">
-            <label for="nombre" class="form-label">Nombre de la Tienda</label>
-            <input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo htmlspecialchars($tienda['nombre']); ?>" required>
+            <label for="nombre" class="form-label">Nombre del canal</label>
+            <input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo htmlspecialchars($canal['nombre']); ?>" required>
         </div>
         <div class="mb-3">
             <label for="canal" class="form-label">Canal</label>
-            <textarea class="form-control" id="canal_id" name="canal_id" rows="3" required><?php echo htmlspecialchars($tienda['canal_id']); ?></textarea>
+            <textarea class="form-control" id="canal_id" name="canal_id" rows="3" required><?php echo htmlspecialchars($canal['id']); ?></textarea>
         </div>
         <button type="submit" class="btn btn-success">Guardar Cambios</button>
         <a href="/canales" class="btn btn-secondary">Cancelar</a>
